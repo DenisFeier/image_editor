9bb48dcdd62be3eb8ef9d2373c70e5153a8e1fd3
Denis Feier <denisfeier@192-168-0-104.rdsnet.ro>
2024-02-08 20:40:08 +0200

    init

---
+++ App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
